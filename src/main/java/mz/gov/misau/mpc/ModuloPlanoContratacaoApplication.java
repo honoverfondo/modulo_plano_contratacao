9c34dda14de7d2990daa3cd584a046042a76f972
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2022-01-21 15:40:53 +0200

    Comite inicial do modulo de plano de contratacao UGEA-MISAU

---
+++ src/main/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplication.java
@@ -0,0 +1,13 @@
+package mz.gov.misau.mpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloPlanoContratacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloPlanoContratacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.gov.misau.mpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloPlanoContratacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
