6a6297545f3ac4f85494d61e17c23eaacd261765
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2022-01-30 20:26:05 +0200

    Anotar a classe Application do Spring Boot

--- src/main/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplication.java
+++ src/main/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplication.java
@@ -2,8 +2,16 @@ package mz.gov.misau.mpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "mz.gov.misau.mpc.model")
+@ComponentScan(basePackages = {"mz.*"})
+@EnableJpaRepositories(basePackages ="mz.gov.misau.mpc.repository")
+@EnableTransactionManagement
 public class ModuloPlanoContratacaoApplication {
 
 	public static void main(String[] args) {
