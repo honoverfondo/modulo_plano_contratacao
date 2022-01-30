b6e6151ccef47b487ac455050c6bbfc4feb550dd
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2022-01-30 21:05:58 +0200

    Classe de teste para ver se esta salvando na base de dados

--- src/test/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplicationTests.java
+++ src/test/java/mz/gov/misau/mpc/ModuloPlanoContratacaoApplicationTests.java
@@ -1,13 +1,28 @@
 package mz.gov.misau.mpc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ModuloPlanoContratacaoApplicationTests {
+import mz.gov.misau.mpc.controller.AcessoController;
+import mz.gov.misau.mpc.model.Acesso;
+import mz.gov.misau.mpc.repository.AcessoRepository;
+import mz.gov.misau.mpc.service.AcessoService;
 
+@SpringBootTest(classes = ModuloPlanoContratacaoApplication.class)
+public class ModuloPlanoContratacaoApplicationTests {
+
+	
+	@Autowired
+	private AcessoController  acessoController;
+	
 	@Test
-	void contextLoads() {
+	public void testCadastrarAcesso() {
+		Acesso acesso=new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvaAcesso(acesso);
 	}
 
 }
