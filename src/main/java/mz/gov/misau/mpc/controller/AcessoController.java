9ea477ddc966e53fe1d45e111b9189cab9d62eb9
honoverfondo <56926254+honoverfondo@users.noreply.github.com>
2022-02-05 20:21:26 +0200

    Resolvendo o erro de dependencias

--- src/main/java/mz/gov/misau/mpc/controller/AcessoController.java
+++ src/main/java/mz/gov/misau/mpc/controller/AcessoController.java
@@ -1,22 +1,43 @@
 package mz.gov.misau.mpc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import mz.gov.misau.mpc.model.Acesso;
-
+import mz.gov.misau.mpc.repository.AcessoRepository;
 import mz.gov.misau.mpc.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
+	
+	@Autowired
+	private AcessoRepository acessoRepository;
 
-	@PostMapping("/salvarAcesso")
-	public Acesso salvaAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@ResponseBody /* PODER DAR UM RETORNO DA API */
+	@PostMapping(value = "**/salvarAcesso") /* MAPEANDO A url PARA RECEBER JSON */
+	public ResponseEntity<Acesso> salvaAcesso(@RequestBody Acesso acesso) {
+		
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 
+	@ResponseBody /* PODER DAR UM RETORNO DA API */
+	@PostMapping(value = "**/deleteAcesso") /* MAPEANDO A url PARA RECEBER JSON */
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) {
+		
+		acessoRepository.deleteById(acesso.getId());
+		
+		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
+	}
 }
--- src/main/java/mz/gov/misau/mpc/repository/AcessoRepository.java
+++ src/main/java/mz/gov/misau/mpc/repository/AcessoRepository.java
@@ -1,12 +1,17 @@
 package mz.gov.misau.mpc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import mz.gov.misau.mpc.model.Acesso;
+
 @Repository
 @Transactional
-public interface AcessoRepository extends JpaRepository<Acesso, Long>{
+public interface AcessoRepository extends JpaRepository<Acesso, Long> {
 
+	
 }
